3b617f5582522059e3290f647b782cb1c672a670
Gabriel Badila <gabrielalin9@yahoo.com>
2017-11-19 16:18:26 +0200

    Little changes in the serial version of Cholesky Algorithm

--- Proiect/serialCholesky.c
+++ Proiect/serialCholesky.c
@@ -2,26 +2,31 @@
 #include <stdlib.h>
 #include <math.h>
 
-double *cholesky(double *a, int n) {
+void cholesky(double *a, int n) {
+    int i, j, k;
     double *L = (double*)calloc(n * n, sizeof(double));
     if (a == NULL)
         exit(EXIT_FAILURE);
 
-    for(int k=0;k<n;k++) {
-      a[k*n+k] = sqrt(a[k*n+k]);
+    for(k = 0; k < n; k++) {
+        a[k * n + k] = sqrt(a[k * n + k]);
 
-      for(int i=k+1;i<n;i++) {
-        a[i*n+k] = a[i*n+k]/a[k*n+k];
-      }
-
-      for(int j=k+1;j<n;j++) {
-        for(int i=j;i<n;i++) {
-          a[i*n+j] = a[i*n+j] - a[i*n+k]*a[j*n+k];
+        for(i = k + 1; i < n; i++) {
+            a[i * n + k] = a[i * n + k] / a[k * n + k];
         }
-      }
+
+        for(j = k + 1; j < n; j++) {
+            for(i = j; i < n; i++) {
+                a[i * n + j] = a[i * n + j] - a[i * n + k] * a[j * n + k];
+            }
+        }
     }
 
-    return L;
+    for (i = 0; i < n; i++) {
+        for (j = i+1; j < n; j++) {
+            a[i*n + j] = 0;
+        }
+    }
 }
 
 void show_matrix(double *A, int n) {
@@ -37,19 +42,17 @@ int main() {
     double m1[] = {25, 15, -5,
                    15, 18,  0,
                    -5,  0, 11};
-    double *c1 = cholesky(m1, n);
+    cholesky(m1, n);
     show_matrix(m1, n);
     printf("\n");
-    free(c1);
 
     n = 4;
     double m2[] = {18, 22,  54,  42,
                    22, 70,  86,  62,
                    54, 86, 174, 134,
                    42, 62, 134, 106};
-    double *c2 = cholesky(m2, n);
+    cholesky(m2, n);
     show_matrix(m2, n);
-    free(c2);
 
     return 0;
 }
